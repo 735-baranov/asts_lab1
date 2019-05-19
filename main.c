115a9ef0e1ace38c847447091d550232b17fc603
Dima Baranov <baranov19972015@gmail.com>
2019-05-19 21:01:16 +0700

    add needed files

---
+++ main.c
@@ -0,0 +1,52 @@
+ /*******************************************************************************
+ *
+ * Copyright (C) 2019 TUSUR, FB, Baranov Dmitriy
+ *
+ * File              : main.c
+ * Compiler          : IAR for ARM 
+ * Version           : 1.0001
+ * Created File      : 14.03.2019
+ * Last modified     : 14.04.2019
+ *
+ * Support mail      : baranov19972015@gmail.com
+ * Target MCU        : MCU: Milandr 1986BE9x
+ *
+ * Description       : Set\reset the desired bit in various ways.
+ *
+********************************************************************************/
+
+#include "bit_band.h"
+#include <stdlib.h>
+#include <stdint.h>
+
+
+struct bitField{
+  int flag_a : 1; 
+  int flag_b : 1;
+  int flag_c : 1; 
+  int flag_d : 1;
+  int flag_e : 1; 
+  int flag_f : 1;
+  int flag_g : 1; 
+  int flag_h : 1;
+} _bitField;
+
+int main(int argc, char *argv[]) {
+  
+  //bit-field
+  _bitField.flag_a = 1;
+  _bitField.flag_a = 0;
+  
+  static uint32_t a = 0;
+  
+  //bit-band realization
+  BIT_BAND_SRAM(&a, 1) = 1; //set 1-st bit in "a" var
+  BIT_BAND_SRAM(&a, 1) = 0; //reset 1-st bit in "a" var
+  
+  
+  //handwork with bits
+  a |= (1<<1); //set 1-st bit in "a" var
+  a &=~(1<<1); //reset 1-st bit in "a" var
+  
+  return EXIT_SUCCESS;
+}
